50e709b3fe806328652ae94588ff421d8e1c3a7f
raffareis <raffareis@gmail.com>
2022-09-10 11:24:23 -0300

    ajuste erro faltando service

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -2,7 +2,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
-import { OpenApiServiceService } from "./open-api-service.service";
 import { FormsModule } from "@angular/forms";
 
 import AppComponent from "./app.component";
@@ -10,8 +9,8 @@ import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule,CommonModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, CommonModule],
   bootstrap: [AppComponent],
-  providers: [HttpClient, OpenApiServiceService],
+  providers: [HttpClient],
 })
-export default class AppModule { }
+export default class AppModule {}
